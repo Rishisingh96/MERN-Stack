000fe9effdb7b3b95ff9e916007519d9eb5defb4
rishisingh <rishicoding9838@gmail.com>
2025-05-25 19:52:58 +0530

    EcmaScript

--- JavaScript Part 2 Advance/ArrayListInitialization.java
+++ JavaScript Part 2 Advance/ArrayListInitialization.java
@@ -1,50 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+// Java Program to Illustrate Address Class
 
-public class ArrayListInitialization {
+package com.geeksforgeeks.org;
 
-    public static void main(String[] args) {
+public class Address {
+    private String houseNo;
+    private String pincode;
+    private String state;
+    private String country;
 
-        // 1️⃣ Using Arrays.asList()
-        ArrayList<String> list1 = new ArrayList<>(Arrays.asList("Java", "Python", "C++"));
-        System.out.println("1. Using Arrays.asList():");
-        for (String lang : list1) {
-            System.out.println("- " + lang);
-        }
+    // Constructor of Address Class
+    public Address(String houseNo, String pincode,
+                   String state, String country)
+    {
+        // Super keyword refers to parent class
+        super();
+        // This keyword refers to current instance itself
+        this.houseNo = houseNo;
+        this.pincode = pincode;
+        this.state = state;
+        this.country = country;
+    }
 
-        // ------------------------------------------------------
-
-        // 2️⃣ Using normal way (adding one by one)
-        ArrayList<String> list2 = new ArrayList<>();
-        list2.add("HTML");
-        list2.add("CSS");
-        list2.add("JavaScript");
-        System.out.println("\n2. Using normal way (add elements one by one):");
-        for (String tech : list2) {
-            System.out.println("- " + tech);
-        }
-
-        // ------------------------------------------------------
-
-        // 3️⃣ Using Anonymous Inner Class
-        ArrayList<String> list3 = new ArrayList<String>() {
-            {
-                add("Spring");
-                add("Hibernate");
-                add("Microservices");
-            }
-        };
-        System.out.println("\n3. Using Anonymous Inner Class:");
-        for (String framework : list3) {
-            System.out.println("- " + framework);
-        }
-
-        // ------------------------------------------------------
-
-        // BONUS: Printing all lists together
-        System.out.println("\n✅ Summary:");
-        System.out.println("Languages: " + list1);
-        System.out.println("Web Tech: " + list2);
-        System.out.println("Frameworks: " + list3);
+    // Method
+    public String toString()
+    {
+        return "[" + houseNo + "," + pincode + "," + state
+            + "," + country + "]";
     }
 }
